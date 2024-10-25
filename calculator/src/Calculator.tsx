5dbd3a5819591143d399db94b6ffd84452dad117
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-10-25 20:41:01 +0300

    refactor: move constant with button symbold from calculator component to consts file

--- calculator/src/Calculator.tsx
+++ calculator/src/Calculator.tsx
@@ -1,5 +1,6 @@
 import type React from 'react';
 import { useState } from 'react';
+import { buttonSymbols } from './shared/consts';
 
 export const Calculator: React.FC = () => {
   const [display, setDisplay] = useState('');
@@ -65,29 +66,6 @@ export const Calculator: React.FC = () => {
       }
     }
   };
-
-  const buttonSymbols: string[] = [
-    '%',
-    '^',
-    '\u221a',
-    '\u00f7',
-    '7',
-    '8',
-    '9',
-    '\u00d7',
-    '4',
-    '5',
-    '6',
-    '-',
-    '1',
-    '2',
-    '3',
-    '+',
-    '0',
-    '.',
-    'C',
-    '=',
-  ];
 
   return (
     <div className="calculator">
